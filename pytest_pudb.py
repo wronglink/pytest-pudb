4b3af1c38aa46e15017c4c65ebee5fa90b94954e
Michael Elovskikh <wronglink@yandex-team.ru>
2018-10-25 12:54:27 +0500

    Refactor code and mount pudb debugger wrapptin to pudb._get_debugger

--- pytest_pudb.py
+++ pytest_pudb.py
@@ -20,10 +20,11 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    if config.getvalue("usepudb"):
-        config.pluginmanager.register(PuDBInvoke(), 'pudbinvoke')
-
     pudb_wrapper = PuDBWrapper(config)
+
+    if config.getvalue("usepudb"):
+        config.pluginmanager.register(pudb_wrapper, 'pudb_wrapper')
+
     pudb_wrapper.mount()
     config._cleanup.append(pudb_wrapper.unmount)
 
@@ -51,43 +52,60 @@ class PuDBWrapper(object):
         if self.pluginmanager is not None:
             capman = self.pluginmanager.getplugin("capturemanager")
             if capman:
-                _suspend_capture(capman, in_=True)
+                outerr = self._suspend_capture(capman, in_=True)
+                if outerr:
+                    out, err = outerr
+                    sys.stdout.write(out)
+                    sys.stdout.write(err)
             tw = self.pluginmanager.getplugin("terminalreporter")._tw
             tw.line()
-            tw.sep(">", "PuDB set_trace (IO-capturing turned off)")
+            tw.sep(">", "entering PuDB (IO-capturing turned off)")
             self.pluginmanager.hook.pytest_enter_pdb(config=self.config)
 
     def _get_debugger(self, **kwargs):
         self.disable_io_capture()
         return self._pudb_get_debugger.__call__(**kwargs)
 
-
-class PuDBInvoke(object):
     def pytest_exception_interact(self, node, call, report):
-        capman = node.config.pluginmanager.getplugin("capturemanager")
-        if capman:
-            out, err = _suspend_capture(capman, in_=True)
-            sys.stdout.write(out)
-            sys.stdout.write(err)
+        """
+        Pytest plugin interface for exception handling
+        https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_exception_interact
+        """
+        self.disable_io_capture()
         _enter_pudb(node, call.excinfo, report)
 
     def pytest_internalerror(self, excrepr, excinfo):
+        """
+        Pytest plugin interface for internal errors handling
+        https://docs.pytest.org/en/latest/reference.html#_pytest.hookspec.pytest_internalerror
+        """
         for line in str(excrepr).split("\n"):
             sys.stderr.write("INTERNALERROR> %s\n" %line)
             sys.stderr.flush()
         tb = _postmortem_traceback(excinfo)
         post_mortem(tb, excinfo)
 
+    def _suspend_capture(self, capman, *args, **kwargs):
+        if hasattr(capman, 'suspendcapture'):
+            # pytest changed the suspend capture API since v3.3.1
+            # see: https://github.com/pytest-dev/pytest/pull/2801
+            # TODO: drop this case after pytest v3.3.1+ is minimal required
+            warnings.warn('You are using the outdated version of pytest. '
+                          'The support for this version will be dropped in the future pytest-pudb versions.',
+                          DeprecationWarning)
+            return capman.suspendcapture(*args, **kwargs)
+
+        if not hasattr(capman, 'snap_global_capture'):
+            # pytest split suspend_global_capture into 2 calls since v3.7.3
+            # see: https://github.com/pytest-dev/pytest/pull/3832
+            # TODO: drop this case after pytest v3.7.3+ is minimal required
+            return capman.suspend_global_capture(*args, **kwargs)
+
+        capman.suspend_global_capture(*args, **kwargs)
+        return capman.read_global_capture()
+
 
 def _enter_pudb(node, excinfo, rep):
-    # XXX we re-use the TerminalReporter's terminalwriter
-    # because this seems to avoid some encoding related troubles
-    # for not completely clear reasons.
-    tw = node.config.pluginmanager.getplugin("terminalreporter")._tw
-    tw.line()
-    tw.sep(">", "traceback")
-    rep.toterminal(tw)
-    tw.sep(">", "entering PuDB")
     tb = _postmortem_traceback(excinfo)
     post_mortem(tb, excinfo)
     rep._pdbshown = True
@@ -105,7 +123,7 @@ def _postmortem_traceback(excinfo):
 
 
 def post_mortem(tb, excinfo):
-    dbg = Debugger()
+    dbg = pudb._get_debugger()
     stack, i = dbg.get_stack(None, tb)
     dbg.reset()
     i = _find_last_non_hidden_frame(stack)
@@ -117,23 +135,3 @@ def _find_last_non_hidden_frame(stack):
     while i and stack[i][0].f_locals.get("__tracebackhide__", False):
         i -= 1
     return i
-
-
-def _suspend_capture(capman, *args, **kwargs):
-    if hasattr(capman, 'suspendcapture'):
-        # pytest changed the suspend capture API since v3.3.1
-        # see: https://github.com/pytest-dev/pytest/pull/2801
-        # TODO: drop this case after pytest v3.3.1+ is minimal required
-        warnings.warn('You are using the outdated version of pytest. '
-                      'The support for this version will be dropped in the future pytest-pudb versions.',
-                      DeprecationWarning)
-        return capman.suspendcapture(*args, **kwargs)
-
-    if not hasattr(capman, 'snap_global_capture'):
-        # pytest split suspend_global_capture into 2 calls since v3.7.3
-        # see: https://github.com/pytest-dev/pytest/pull/3832
-        # TODO: drop this case after pytest v3.7.3+ is minimal required
-        return capman.suspend_global_capture(*args, **kwargs)
-
-    capman.suspend_global_capture(*args, **kwargs)
-    return capman.read_global_capture()
